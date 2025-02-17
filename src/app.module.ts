ac0bfb23a425cba0fe7d410eb4f76d1e1df846cb
Solomka Andrii <andriissolomka@gmail.com>
2025-02-17 13:25:23 +0200

    Add Authentication & Authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CookieModule } from './cookie/cookie.module';
+import { EmailModule } from './email/email.module';
+import { ScheduleModule } from './schedule/schedule.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, AuthModule, CookieModule, EmailModule, ScheduleModule],
 })
 export class AppModule {}
