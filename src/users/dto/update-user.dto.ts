3d37f260eabe385b59bf6e2285bf71b9e84be78e
Solomka Andrii <andriissolomka@gmail.com>
2025-02-18 17:45:27 +0200

    added admin

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,9 @@
+import { IsBoolean, IsOptional } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 import { PartialType } from '@nestjs/mapped-types';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @IsBoolean()
+  is_blocked: boolean;
+}
